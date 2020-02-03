7dbc974c6fcbe417d6dfed05700f2613cd33ef29
ywra <nyw0242@etri.re.kr>
2020-02-03 14:54:37 +0900

    modify

--- src/main.go
+++ src/main.go
@@ -5,9 +5,34 @@ import (
 )
 
 func main() {
-	buff := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	fmt.Println(buff)
-	for val := range buff {
-		fmt.Println(val)
+	var temp string
+	temp = "hello"
+	fmt.Println(temp)
+	var tvt [5]byte
+	tt := []byte(temp)
+	for index, val := range tt {
+		fmt.Println(index, val)
+		tvt[index] = val
 	}
+
+	fmt.Println(tt)
+	checkBuffer(tt)
+	fmt.Println(tt)
+
+	fmt.Println(tvt)
+	checkBufferCopy(tvt)
+	fmt.Println(tvt)
+
+	ttt := [...]byte{tvt[0], tvt[1], tvt[2], tvt[3], tvt[4]}
+	fmt.Println("ttt: : ", ttt)
+	checkBufferCopy(ttt)
+	fmt.Println("ttt: ", ttt)
+}
+
+func checkBuffer(temp []byte) {
+	temp[0] = 1
+}
+
+func checkBufferCopy(temp [5]byte) {
+	temp[0] = 1
 }
